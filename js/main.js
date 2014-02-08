ffb5112e037002fdcb5ea60c37d7e89e1d66d8cc
Rafa Latorre <sauco82@gmail.com>
2014-02-08 16:07:56 +0100

    basic ball animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+function createBall(target, type, points) {	
+	var ball = $('<span class="ball"></span>').appendTo("div#gamedev"),
+		position = {
+			top: target.position().top - 25*Math.random(),
+			left: target.position().left - 25*Math.random()
+		},
+		typeClass = "ball--" + type;
 
+	ball.css({
+		top:position.top,
+		left:position.left,
+	});
+
+	ball.addClass(typeClass);
+
+	return ball
+}
