1ecdfcf277c98fa4103d2767a1f3626a83976520
Rafa Latorre <sauco82@gmail.com>
2014-02-09 13:33:54 +0100

    added support for heroku deploy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,65 @@
+var gameConfig = {
+	speed: 1,
+	time: 0,
+	pause: false
+};
+
+function gameLoop() {
+	console.log("working")
+	if (!gameConfig.pause) {
+		addRandomBall();
+	} 
+	setTimeout(gameLoop, 3000);
+}
+
+function pause() { 
+	gameConfig.pause = true 
+}
+
+function cancelPause() { 
+	gameConfig.pause = false 
+}
+
+// ========================================
+	// Notifications
+// ========================================
+function startGame() {
+	popupToNotifications();
+	gameLoop();
+	$("#black-screen").hide();
+}
+
+function popupToNotifications() {
+	var content = $("#popup .popup__content").html(),		
+		notification = $('<li class="notification"></li>').appendTo("#notifications");
+
+	notification.html(content);
+	$("#popup").addClass("to-notifications");
+	
+	setTimeout(function(){
+		$("#popup").remove();
+	},300)
+}
+
+
+// ========================================
+	// Ball dinamics
+// ========================================
+function addRandomBall(){
+	var characters = ["#char1","#char2","#lead"],
+		types = ["design","development","bugs","research"],
+		points = Math.round(3*Math.random()) + 1,
+		charId = Math.floor(characters.length*Math.random()),
+		typeId = Math.floor(types.length*Math.random());
+
+
+	createBall(
+		$(characters[charId]),
+		types[typeId],
+		points
+	);
+}
+
 function createBall(target, type, points) {	
 	var ball = $('<span class="ball"></span>').appendTo("div#gamedev"),
 		position = {
@@ -13,5 +75,44 @@ function createBall(target, type, points) {
 
 	ball.addClass(typeClass);
 
-	return ball
+	ball.html(points)
+	
+	setTimeout( function() {moveBallToProgress(ball, type)}, 1000);
+}
+
+function moveBallToProgress(ball, target) {
+	var type = $("#"+target),
+		position = type.position();
+
+	ball.css({
+		top: position.top,
+		left: position.left + parseInt($(".progress").css('margin-left'))
+	});	
+
+	setTimeout(function(){
+		var newResult = parseInt(type.html())+parseInt(ball.html());
+		
+		type.html(newResult);
+		highlightType(type);
+		ball.remove()
+	},500)
+}
+
+function highlightType(type) {
+	var highlightClass = "highlight__" + type.attr('id'),
+		position = {
+			top: type.position().top,
+			left: type.position().left
+		},
+		highlight = $('<span class="highlight '+highlightClass+'"></span>').insertBefore(type);
+	
+	highlight.css({
+		'top': position.top,
+		'left': position.left,		
+	});
+
+	setTimeout(function(){
+		highlight.remove()
+	},300)
+		
 }
